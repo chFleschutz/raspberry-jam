4ae9412df6b5875ab3149352ab152590387b1534
Luca <93133781+Loparde@users.noreply.github.com>
2024-05-31 21:41:14 +0200

    CameraHandling
    fix #21

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public static CameraMovement Instance;
+    [SerializeField] private AnimationCurve speedOverTime;
     [SerializeField] private float speed;
+    private float distance;
     private List<Vector2> goalPositions = new List<Vector2>();
 
     public void AddGoalDirecion(Vector2 direction)
@@ -45,8 +47,11 @@ public class CameraMovement : MonoBehaviour
         Vector3 direction = new Vector3(goalPositions[0].x, goalPositions[0].y, 0) - new Vector3(transform.position.x, transform.position.y, 0);
 
         if (!(direction.x < 0.01f && direction.x > -0.01f) || !(direction.y < 0.01f && direction.y > -0.01f))
-            transform.position += direction.normalized * speed * Time.deltaTime;
+            transform.position += direction.normalized * speed * Time.deltaTime * speedOverTime.Evaluate(direction.magnitude/distance);
         else
+        {
             goalPositions.RemoveAt(0);
+            distance = (new Vector3(goalPositions[0].x, goalPositions[0].y, 0) - transform.position).magnitude;
+        }
     }
 }
