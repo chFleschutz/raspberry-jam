777ae612fb52891fdd7c06683f2594cf2183adb2
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-05-31 17:54:18 +0200

    Add health skript and player damage and death events

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1000f)] private float maxHealth = 100.0f;
+    [SerializeField] private GameEventFloat onHealthChanged;
+    [SerializeField] private GameEvent onDeath;
+
+    private float health = 100.0f;
+
+    public void TakeDamage(float damage)
+    {
+        if (health <= 0)
+            return;
+
+        health -= damage;
+        if (health <= 0)
+        {
+            onDeath.Invoke(this);
+        }
+
+        onHealthChanged.Invoke(this, health);
+    }
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+}
