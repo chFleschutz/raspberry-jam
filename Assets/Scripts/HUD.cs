3bfdeb4accb86ec9f2f3ab203194c8503116ece7
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-06-01 13:06:46 +0200

    Add Healthbar to HUD
    Resolve #49

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUD : MonoBehaviour, IGameEventListener<int>
+public class HUD : MonoBehaviour, IGameEventListener<int>, IGameEventListener<float>
 {
     [Header("Events")]
     [SerializeField] private GameEvent<int> scoreEvent;
+    [SerializeField] private GameEvent<float> healthEvent;
 
     [Header("HUD Elements")]
     [SerializeField] private TMPro.TextMeshProUGUI scoreText;
+    [SerializeField] private HealthBar healthBar;
 
+    // Score Event
     public void OnInvoke(int parameter)
     {
         scoreText.text = "Score: " + parameter.ToString();
     }
 
+    // Health Event
+    public void OnInvoke(float parameter)
+    {
+        healthBar.SetHealth(parameter / 100); // TODO: Use MaxHealth of the Player instead of 100
+    }
+
     void Start()
     {
         scoreEvent.RegisterListener(this);
+        healthEvent.RegisterListener(this);
     }
 
     void OnDestroy()
     {
         scoreEvent.UnregisterListener(this);
+        healthEvent.UnregisterListener(this);
     }
 }
